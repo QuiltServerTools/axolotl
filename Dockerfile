259852810cde51135e7b89fc002e02277479a59b
Tom_The_Geek <tomthegeek.8559@gmail.com>
2021-08-02 21:28:36 +0100

    Include tags in docker image, rename image to axolotl and remove check that kordex already does

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,6 @@ COPY build/libs/bot-*-all.jar /usr/local/lib/bot.jar
 RUN mkdir /bot
 WORKDIR /bot
 
+COPY tags /bot/tags
+
 ENTRYPOINT [ "java", "-Xms1G", "-Xmx1G", "-jar", "/usr/local/lib/bot.jar" ]
--- src/main/kotlin/io/github/quiltservertools/bot/_checks.kt
+++ src/main/kotlin/io/github/quiltservertools/bot/_checks.kt
@@ -8,17 +8,5 @@ import dev.kord.common.annotation.KordPreview
 import dev.kord.common.entity.Snowflake
 
 fun SlashCommand<*>.onlyModerator() {
-    check {
-        val member = memberFor(event)
-        if (member == null) {
-            fail("Not in a server!")
-        } else {
-            if (member.asMember().roleIds.none { it == MODERATOR_ROLE }) {
-                fail("You don't have the right permissions")
-            }
-        }
-    }
-
-    // Discord says not to trust their role checks, so we have a manual check above too.
     allowRole(MODERATOR_ROLE)
 }
