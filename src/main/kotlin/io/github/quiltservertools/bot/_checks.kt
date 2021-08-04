5340da141f924cb2c2c06f039d6cc9076dc2e44f
Tom_The_Geek <tomthegeek.8559@gmail.com>
2021-08-04 16:50:59 +0100

    Unused imports be gone!

--- src/main/kotlin/io/github/quiltservertools/bot/_checks.kt
+++ src/main/kotlin/io/github/quiltservertools/bot/_checks.kt
@@ -2,10 +2,8 @@
 
 package io.github.quiltservertools.bot
 
-import com.kotlindiscord.kord.extensions.checks.memberFor
 import com.kotlindiscord.kord.extensions.commands.slash.SlashCommand
 import dev.kord.common.annotation.KordPreview
-import dev.kord.common.entity.Snowflake
 
 fun SlashCommand<*>.onlyModerator() {
     allowRole(MODERATOR_ROLE)
