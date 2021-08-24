0ea952a48277fef43cf0934b7763fd8b5da58c4e
Tom_The_Geek <tomthegeek.8559@gmail.com>
2021-08-24 13:47:30 +0100

    Snazzier logging

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,21 +1,25 @@
 import ch.qos.logback.core.joran.spi.ConsoleTarget
 
-def environment = System.getenv().getOrDefault("ENVIRONMENT", "production")
+def environment = System.getenv().getOrDefault("SENTRY_ENVIRONMENT", "dev")
 def defaultLevel = INFO
 
-if (environment == "debug") {
+if (environment == "dev") {
     defaultLevel = DEBUG
 
     // Silence warning about missing native PRNG on Windows
     logger("io.ktor.util.random", ERROR)
+    // Silence warnings about missing translations (we aren't using them)
+    logger("com.kotlindiscord.kord.extensions.i18n.ResourceBundleTranslations", ERROR)
+    // Silence JGit
+    logger("org.eclipse.jgit", INFO)
 }
 
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{yyyy-MM-dd HH:mm:ss:SSS Z} | %5level | %40.40logger{40} | %msg%n"
+        pattern = "%magenta([%d{YYYY-MM-dd HH:mm:ss.SSS}]) %cyan([%thread]) %highlight(%-5level) %yellow(%logger{36}) - %msg%n"
     }
 
-    target = ConsoleTarget.SystemErr
+    target = ConsoleTarget.SystemOut
 }
 
 root(defaultLevel, ["CONSOLE"])
